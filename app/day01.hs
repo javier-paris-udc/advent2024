5fa65a10b0d32c30af9c734ef38637ccc059d467
Javier París <javier.paris@udc.es>
2024-12-01 15:17:20 +0100

    cleaned up day01

--- app/day01.hs
+++ app/day01.hs
@@ -20,26 +20,25 @@ solveP2 (l1, l2) = timeIds sl1 sl2
         & map (\l -> (head l, length l))
     timeIds [] _ = 0
     timeIds _ [] = 0
-    timeIds (x:xs) yl@((y, times) : ys)
+    timeIds xl@(x:xs) yl@((y, times) : ys)
         | x < y = timeIds xs yl
-        | x > y = timeIds (x:xs) ys
+        | x > y = timeIds xl ys
         | otherwise = x*times + timeIds xs yl
 
 
 solveP1 :: ([Int], [Int]) -> Int
 solveP1 =
         bimap sort sort
-    >>> uncurry (zipWith (\a b -> abs (a - b)))
+    >>> uncurry (zipWith (-))
+    >>> map abs
     >>> sum
 
 
 numListsP :: Parser ([Int], [Int])
-numListsP = do
-    unzip <$> numPairP `sepEndBy` newline
+numListsP = unzip <$> numPairP `sepEndBy` newline
   where
     numPairP = do
-        n1 <- intP
-        spaces
+        n1 <- intP <* spaces
         n2 <- intP
         return (n1, n2)
 
