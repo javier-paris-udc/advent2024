f94c97bfaf0926bdde6c043d5dc2e62e5418a388
Javier París <javier.paris@udc.es>
2024-12-07 12:16:30 +0100

    day07 solution

---
+++ app/day07.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import AoC                (applyInput, blankP, intP)
+import Text.Parsec        (char, sepEndBy1, spaces)
+import Text.Parsec.String (Parser)
+
+
+digits :: Int -> Int
+digits x
+    | abs x < 10 = 1
+    | otherwise  = 1 + digits (x `div` 10)
+
+
+cat :: Int -> Int -> Int
+cat a b = a * 10 ^ digits b + b
+
+
+solvableEq :: [Int -> Int -> Int] -> Int -> [Int] -> Bool
+solvableEq ops x l = case l of
+    []       -> False
+    [y]      -> x == y
+    y1:y2:ys -> any (\op -> solvableEq ops x (y1 `op` y2 : ys)) ops
+
+
+solve :: [Int -> Int -> Int] -> [(Int, [Int])] -> Int
+solve ops = sum . map fst . filter (uncurry (solvableEq ops))
+
+
+equationsP :: Parser [(Int, [Int])]
+equationsP = eqP `sepEndBy1` spaces
+  where
+    eqP = do
+        target <- intP <* char ':' <* spaces
+        vals <- intP `sepEndBy1` blankP
+        return (target, vals)
+
+
+main :: IO ()
+main = applyInput equationsP (solve [(+), (*)]) (solve [(+), (*), cat])
